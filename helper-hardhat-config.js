7260228d7c576e091b17955871213e1a5841542f
ManuWeb3 <manu.web3.0@gmail.com>
2023-01-12 17:09:10 -0700

    Some commit

--- helper-hardhat-config.js
+++ helper-hardhat-config.js
@@ -1,16 +1,7 @@
 const { ethers } = require("hardhat")
 
 const networkConfig = {
-    4: {
-        name: "rinkeby",
-        vrfCoordinatorV2: "0x6168499c0cFfCaCD319c818142124B7A15E857ab",
-        gasLane: "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
-        subscriptionId: "10325",                                                        // temp. assign Zero
-        callbackGasLimit: "500000",                                                     // set high: 500,000 gas units
-        interval: "30",                                                                  // 30 seconds
-        mintFee: "10000000000000000",           // 0.01 ETH
-        ethUsdPriceFeed: "0x8A753747A1Fa494EC906cE90E9f37563A8AF630e",
-    },
+    
     31337: {
         name: "hardhat",
         // vrfCoordinatorV2: NOT needed here because we're deploying mocks on "hardhat" and "localhost"...
